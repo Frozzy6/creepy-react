8c61b66b14cacc861ba07b212327dc641a859017
Anatoly Rybalchenko <Frozzy6@gmail.com>
2018-06-28 23:43:28 +0300

    update user page

--- app/components/UserPage/UserPage.js
+++ app/components/UserPage/UserPage.js
@@ -1,23 +1,66 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import { Map, List } from 'immutable';
 import { Link } from 'react-router-dom';
+import { isNil } from 'lodash';
+import classNames from 'classnames';
 
 import Header from './Header';
+
+const STATUSES = [
+  'В очереди на рассмотрение',
+  'Отклонена',
+  'В рассмотрении',
+  'Опубликована',
+];
 
 const getNoPublicationsMessage = isCurrentUser => (isCurrentUser ?
   <p>Вы пока не сделали ни одной публикации</p> :
   <p>Автор пока не сделал ни одной публикации</p>
 );
 
-const getPublicationItem = publication => (
-  <li key={publication.get('uID')}>
-    <Link to={`/story/${publication.get('uID')}`}>
-      {publication.get('title')}
-    </Link>
-  </li>
-);
+const getPublicationItem = (publication, isCurrentUser) => {
+  // TODO: replace by dict
+  const STATUS_PUBLISHED = 3;
+  const uID = publication.get('uID');
+  const title = publication.get('title');
+  const status = publication.get('status');
+  const likesCount = publication.get('likesCount');
+  const commentsCount = publication.get('commentsCount');
+  const isPublishedStory = status === STATUS_PUBLISHED;
+  const href = (isNil(status) || status === STATUS_PUBLISHED) && `/story/${uID}`;
+
+  return (
+    <li key={uID} className="publication-item">
+      <div>
+        {href ?
+          <Link to={href}>
+            <b>{title}</b>
+          </Link> :
+          <div className="publication-field-title">{title}</div>
+        }
+      </div>
+      <div className="publication-info">
+        {isCurrentUser && !isPublishedStory &&
+          <span
+            className={classNames('publication-field-status', {
+              'publication-field-status_rejected': status === 1,
+            })}
+          >
+            {STATUSES[status]}
+          </span>
+        }
+        { isPublishedStory &&
+          <Fragment>
+            <span className="publication-field-comments"><i className="fa fa-heart" /> {commentsCount}</span>
+            <span className="publication-field-likes"><i className="fa fa-comments" /> {likesCount}</span>
+          </Fragment>
+        }
+      </div>
+    </li>
+  );
+};
 
 class UserPage extends Component {
   handleUpload = (event) => {
@@ -51,9 +94,9 @@ class UserPage extends Component {
           { pubInfo.size === 0 ?
             getNoPublicationsMessage(isCurrentUser) :
             <div>
-              <h2>Публикации:</h2>
-              <ul>
-                {pubInfo.map(item => getPublicationItem(item))}
+              <h2>{isCurrentUser ? 'Ваши публикации:' : 'Публикации:'}</h2>
+              <ul className="publication-list">
+                {pubInfo.map(item => getPublicationItem(item, isCurrentUser))}
               </ul>
             </div>
           }
