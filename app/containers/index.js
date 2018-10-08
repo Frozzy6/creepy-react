9faaeb9abff007ec8b3c423cf872db70ab02356e
Anatoly Rybalchenko <Frozzy6@gmail.com>
2018-10-09 00:30:17 +0300

    Update imports

--- app/containers/index.js
+++ app/containers/index.js
@@ -1,4 +1,4 @@
 //TODO: remove this file
-export AppContainer from './App/AppContainer';
-export ContentContainer from './Content/ContentContainer';
-export SingleContentContainer from './Content/SingleContentContainer';
+export { default as AppContainer } from './App/AppContainer';
+export { default as ContentContainer } from './Content/ContentContainer';
+export { default as SingleContentContainer } from './Content/SingleContentContainer';
